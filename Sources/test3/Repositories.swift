4b0781ec0922752e55fc14ce93762bf4acdd7f68
Artem Anufriev <9o2oooo222@gmail.com>
2021-06-18 00:26:17 +0400

    update

--- Sources/test3/Repositories.swift
+++ Sources/test3/Repositories.swift
@@ -6,54 +6,20 @@
 //
 
 import Foundation
-import Alamofire
 
-class Repositories {
+struct Repositories: Decodable {
+    let id: Int
+    let name: String
     
-    struct Repo: Decodable {
-        let id: Int
-        let name: String
-        
-        enum CodingKeys: String, CodingKey {
-            case id = "id"
-            case name = "name"
-        }
-    }
-    
-    typealias repositoriesArray = [Repo]
-    
-    private var repositories: repositoriesArray
-    private var userName: String
-    private var url: String
-    
-    init(forUser userName: String) {
-        self.userName = userName
-        self.repositories = []
-        self.url = "https://api.github.com/users/\(userName)/repos?per_page=100&page="
-    }
-    
-    public func print() {
-        if !repositories.isEmpty {
-            for index in repositories.indices {
-                Swift.print("\(index + 1) - id: \(repositories[index].id), name: \(repositories[index].name)")
-            }
-            Swift.print("Total of \(repositories.count) repositories for the user \(userName).")
-        } else {
-            Swift.print("There are no repositories for the user \(userName)..")
-        }
-    }
-    
-    public func request(forPage page : Int, semaphore mySemaphore : DispatchSemaphore) {
-        AF.request(url + String(page)).validate().responseDecodable(of: repositoriesArray.self, queue: .global()) { [self] (response) in
-            guard let returnedRepos = response.value, !returnedRepos.isEmpty else {
-                mySemaphore.signal()
-                return
-            }
-            repositories.append(contentsOf: returnedRepos)
-            request(forPage: page + 1,semaphore: mySemaphore)
-        }
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
     }
 }
+
+typealias repositoriesArray = [Repositories]
+
+
 
 
 
--- Sources/test3/main.swift
+++ Sources/test3/main.swift
@@ -1,9 +1,50 @@
 import Foundation
+import Alamofire
+
+var semaphore = DispatchSemaphore(value: 0)
+
+private func request(forURL url : URL, closure: @escaping(repositoriesArray) -> Void) {
+    AF.request(url).validate().responseDecodable(of: repositoriesArray.self, queue: .global()) { response in
+        switch response.result {
+        case .success:
+            closure(response.value ?? [])
+            if let links = response.response?.headers.dictionary["Link"],
+               let nextLink = links.split(separator:Character(",")).first(where: { $0.contains("rel=\"next\"")}) {
+                guard let beginIndex = nextLink.firstIndex(of: Character("<")),
+                      let endIndex = nextLink.firstIndex(of: Character(">")),
+                      let nextUrl = URL(string: String(nextLink[nextLink.index(after: beginIndex)..<endIndex])) else {
+                    return
+                }
+                request(forURL: nextUrl, closure: closure)
+            } else {
+                semaphore.signal()
+            }
+        case let .failure(error):
+            if let errorDescription = error.errorDescription {
+                print(errorDescription)
+            }
+        }
+    }
+}
 
 print("Enter the user name to get the list of repositories and then press enter:")
-var userName: String = readLine()!
-var repositories = Repositories(forUser: userName)
-var semaphore = DispatchSemaphore(value: 0)
-repositories.request(forPage: 1, semaphore: semaphore)
+guard let userName = readLine(),
+      !userName.isEmpty,
+      let url = URL(string: "https://api.github.com/users/\(userName)/repos?per_page=100&page=1") else
+{
+    print("username is empty.")
+    exit(0)
+}
+request(forURL: url) { repos in
+    if repos.isEmpty {
+        print("There are no repositories for the user \(userName)..")
+    }
+    else {
+        repos.forEach { repo in
+            print("id: \(repo.id), name: \(repo.name)")
+        }
+        print("Total of \(repos.count) repositories for the user \(userName).")
+    }
+}
 semaphore.wait()
-repositories.print()
+
